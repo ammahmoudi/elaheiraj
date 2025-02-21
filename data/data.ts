9b7e335524fc59c4dccb933177a0b3912e068a72
ammahmoudi <am.mahmoudi@outlook.com>
2025-02-21 22:54:47 +0330

    inital commit

---
+++ data/data.ts
@@ -0,0 +1,35 @@
+const data = {
+  sitename: "Elahe Hosseini Iraj",
+  sitetagline: "elahe iraj portfolio",
+  sitelogo: "",
+  title: "Comming Soon",
+  description:
+    " This is Where you know Elahe More😊",
+  newsletterheading: "Stay tuned for live updates!",
+  copyrightText: "Copyright © 2025 | A.M",
+  socialIconsHeading: "Follow Elahe 📣",
+  socialIcons: [
+    // {
+    //   icon: "facebook",
+    //   link: "https://www.facebook.com/sandeep.baikare/",
+    // },
+    // {
+    //   icon: "twitter",
+    //   link: "https://twitter.com/BaikareSandeep",
+    // },
+    {
+      icon: "linkedIn",
+      link: "https://www.linkedin.com/in/elaheiraj/",
+    },
+    {
+      icon: "instagram",
+      link: "https://www.instagram.com/its3lahe/",
+    },
+    
+
+
+
+  ],
+};
+
+export default data;
---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from './theme-provider'
+import { ThemeSwitcher } from '../../components/ThemeSwitcher'
+import Head from 'next/head'
+
+const inter = Inter({ 
+  weight: ['100', '200', '300', '600', '400', '700', '900'],
+  subsets: ['latin'] 
+})
+
+export const metadata = {
+  title: "Elahe Hosseini Iraj",
+  description: 'Portfolio of Elahe Iraj',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className={ `${inter.className} bg-gray-200	 dark:bg-[#0d1117] `}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          <main>{children}</main>
+        </ThemeProvider>          
+      </body>
+    </html>
+  )
+}
